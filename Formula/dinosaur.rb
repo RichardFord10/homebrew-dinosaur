dbf5f873757da41f163aa58cc209196ee8f9d2e7
Richard Ford <richardford10@gmail.com>
2024-12-27 11:38:18 -0500

    update url

--- Formula/dinosaur.rb
+++ Formula/dinosaur.rb
@@ -2,7 +2,7 @@
 class Dinosaur < Formula
   desc "Print a friendly ASCII dinosaur"
   homepage "https://github.com/richardford10/dinosaur"
-  url "https://github.com/richardford10/dinosaur/archive/v0.1.0.tar.gz"
+  url "https://github.com/RichardFord10/dinosaur/archive/refs/tags/0.1.0.tar.gz"
   sha256 "37cd1315be3a19d68f0b029dd7d2baa6e3c6408a6c86abb607216c8ad06abc5a"
   license "MIT"
 
